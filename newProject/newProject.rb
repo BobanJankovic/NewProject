4d445ec4e9091e4a1f6b65db1971bd5fb8587842
Boban Jankovic <36967070+BobanJankovic@users.noreply.github.com>
2018-03-19 19:19:50 +0100

    Add files via upload

---
+++ newProject/newProject.rb
@@ -0,0 +1,25 @@
+puts "Please name your new Project?"
+projectName = gets.strip
+
+puts(projectName);
+
+puts 'mkdir #{projectName}'
+puts 'cd #{projectName}'
+puts 'touch index.html'
+
+puts 'mkdir css'
+puts 'touch main.css'
+puts 'cd ..'
+
+puts 'mkdir js'
+puts 'touch app.js'
+puts 'cd ..'
+
+puts 'mkdir images'
+
+
+puts 'git init'
+puts 'git add .'
+puts 'git commit -m 'First Commit for #{projectName}''
+puts 'git push origin master'
+puts 'clear'
